ee4be989513c7c9c9a73498fb7e9d79bb969812d
Magnus Ulimoen <magnus@ulimoen.dev>
2021-01-31 12:49:16 +0100

    rename matrix mul tests

--- tests/ops.rs
+++ tests/ops.rs
@@ -229,6 +229,46 @@ fn matrix_sub() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+// Matrix * Matrix
+////////////////////////////////////////////////////////////////////////////////
+
+#[test]
+fn matrix_mul_dot_product() {
+    let a = matrix![-1, 3, -3, 7];
+    let b = matrix![-4; 4; 5; 2];
+    let c = matrix![15];
+
+    for_each_op_assert_eq! { a, *, b, c }
+}
+
+#[test]
+fn matrix_mul_n_by_n() {
+    let a = matrix![1, 2; 4, 5];
+    let b = matrix![6, 7; 8, 9];
+    let c = matrix![22, 25; 64, 73];
+
+    for_each_op_assert_eq! { a, *, b, c }
+}
+
+#[test]
+fn matrix_mul_n_by_m() {
+    let a = matrix![1, 2, 3; 4, 5, 6];
+    let b = matrix![6, 7, 8, 9; 10, 11, 12, 13; 14, 15, 16, 17];
+    let c = matrix![68, 74, 80, 86; 158, 173, 188, 203];
+
+    for_each_op_assert_eq! { a, *, b, c }
+}
+
+#[test]
+fn matrix_mul_0_by_m() {
+    let a = Matrix::<_, 0, 3>::default();
+    let b = matrix![6, 7, 8, 9; 10, 11, 12, 13; 14, 15, 16, 17];
+    let c = Matrix::default();
+
+    for_each_op_assert_eq! { a, *, b, c }
+}
+
+////////////////////////////////////////////////////////////////////////////////
 // Matrix += Matrix
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -264,32 +304,4 @@ fn matrix_not() {
     let a = matrix![1, -3; 3, -7];
     assert_eq!(!a, matrix![-2, 2; -4, 6]);
     assert_eq!(!&a, matrix![-2, 2; -4, 6]);
-}
-
-mod matrix_multiplication {
-    use super::*;
-    #[test]
-    fn dot_product() {
-        let a = matrix![-1, 3, -3, 7];
-        let b = matrix![-4; 4; 5; 2];
-        let c = matrix![15];
-
-        for_each_op_assert_eq! { a, *, b, c }
-    }
-    #[test]
-    fn n_by_n() {
-        let a = matrix![1, 2; 4, 5];
-        let b = matrix![6, 7; 8, 9];
-        let c = matrix![22, 25; 64, 73];
-
-        for_each_op_assert_eq! { a, *, b, c }
-    }
-    #[test]
-    fn n_by_m() {
-        let a = matrix![1, 2, 3; 4, 5, 6];
-        let b = matrix![6, 7, 8, 9; 10, 11, 12, 13; 14, 15, 16, 17];
-        let c = matrix![68, 74, 80, 86; 158, 173, 188, 203];
-
-        for_each_op_assert_eq! { a, *, b, c }
-    }
 }
