1f2e4455e07b5923d501d08eb268488bf9b15911
Magnus Ulimoen <magnus@ulimoen.dev>
2021-01-31 12:51:07 +0100

    move matrix mul below matrix add

--- src/ops.rs
+++ src/ops.rs
@@ -155,6 +155,45 @@ impl_op! { Add, add }
 impl_op! { Sub, sub }
 
 ////////////////////////////////////////////////////////////////////////////////
+// Matrix * Matrix
+////////////////////////////////////////////////////////////////////////////////
+
+fn matrix_mul<T, const N: usize, const M: usize, const P: usize>(
+    lhs: &Matrix<T, N, M>,
+    rhs: &Matrix<T, M, P>,
+) -> Matrix<T, N, P>
+where
+    T: Copy + Default + Add<Output = T> + Mul<Output = T> + core::iter::Sum,
+{
+    let mut out = Matrix::default();
+    for (j, out) in out.data.iter_mut().enumerate() {
+        for (i, out) in out.iter_mut().enumerate() {
+            *out = (0..M).map(|k| lhs[(i, k)] * rhs[(k, j)]).sum();
+        }
+    }
+    out
+}
+
+macro_rules! impl_op_mul_mul {
+    ($lhs:ty, $rhs:ty) => {
+        impl<T, const N: usize, const M: usize, const P: usize> Mul<$rhs> for $lhs
+        where
+            T: Copy + Default + Add<Output = T> + Mul<Output = T> + core::iter::Sum,
+        {
+            type Output = Matrix<T, N, P>;
+            fn mul(self, rhs: $rhs) -> Self::Output {
+                matrix_mul(&self, &rhs)
+            }
+        }
+    };
+}
+
+impl_op_mul_mul! {  Matrix<T, N, M>,  Matrix<T, M, P> }
+impl_op_mul_mul! { &Matrix<T, N, M>,  Matrix<T, M, P> }
+impl_op_mul_mul! {  Matrix<T, N, M>, &Matrix<T, M, P> }
+impl_op_mul_mul! { &Matrix<T, N, M>, &Matrix<T, M, P> }
+
+////////////////////////////////////////////////////////////////////////////////
 // Matrix += Matrix
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -201,42 +240,3 @@ impl_op_unary! { impl Neg, neg for  Matrix<T, M, N> }
 impl_op_unary! { impl Neg, neg for &Matrix<T, M, N> }
 impl_op_unary! { impl Not, not for  Matrix<T, M, N> }
 impl_op_unary! { impl Not, not for &Matrix<T, M, N> }
-
-////////////////////////////////////////////////////////////////////////////////
-// Matrix * Matrix
-////////////////////////////////////////////////////////////////////////////////
-
-fn matrix_mul<T, const N: usize, const M: usize, const P: usize>(
-    lhs: &Matrix<T, N, M>,
-    rhs: &Matrix<T, M, P>,
-) -> Matrix<T, N, P>
-where
-    T: Copy + Default + Add<Output = T> + Mul<Output = T> + core::iter::Sum,
-{
-    let mut out = Matrix::default();
-    for (j, out) in out.data.iter_mut().enumerate() {
-        for (i, out) in out.iter_mut().enumerate() {
-            *out = (0..M).map(|k| lhs[(i, k)] * rhs[(k, j)]).sum();
-        }
-    }
-    out
-}
-
-macro_rules! impl_op_mul_mul {
-    ($lhs:ty, $rhs:ty) => {
-        impl<T, const N: usize, const M: usize, const P: usize> Mul<$rhs> for $lhs
-        where
-            T: Copy + Default + Add<Output = T> + Mul<Output = T> + core::iter::Sum,
-        {
-            type Output = Matrix<T, N, P>;
-            fn mul(self, rhs: $rhs) -> Self::Output {
-                matrix_mul(&self, &rhs)
-            }
-        }
-    };
-}
-
-impl_op_mul_mul! { Matrix<T, N, M>, Matrix<T, M, P> }
-impl_op_mul_mul! { &Matrix<T, N, M>, Matrix<T, M, P> }
-impl_op_mul_mul! { Matrix<T, N, M>, &Matrix<T, M, P> }
-impl_op_mul_mul! { &Matrix<T, N, M>, &Matrix<T, M, P> }
